d202fefff26ad68e8b3d277a94e34527ff722973
Pratchaya Yordming <pe@appsynth.net>
2024-12-20 21:14:20 +0700

    update

---
+++ src/app/mobile/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link"; // เพิ่มการ import Link
+
+export default function Mobile() {
+    return (
+      <main>
+        <h1>Download Page</h1>
+        <Link href="/" className="text-blue-600 hover:underline">Back to Home</Link>
+      </main>
+    );
+  }
